f7798d69e4d41b7fb7f5e3b90a5a5f5c8ea6ed66
Dev. Khanh <nguyenphiikhanh@gmail.com>
2023-08-20 22:13:07 +0700

    config t2ico icon-font

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <i class="t2ico t2ico-wallet"></i>
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
