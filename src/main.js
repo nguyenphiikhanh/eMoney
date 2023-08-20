c414b269276bd185b9c747dafcebf7267c9dca30
Dev. Khanh <nguyenphiikhanh@gmail.com>
2023-08-20 22:29:35 +0700

    config router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from "./router/index.js";
 
 import "./assets/styles/css/tailwind.css";
 import "./assets/styles/css/global.css";
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
