5fd0e471d5038f208069f0780d2e85fbe277fac2
Dev. Khanh <nguyenphiikhanh@gmail.com>
2023-08-31 11:12:53 +0700

    redirect register

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,10 +1,19 @@
+<template>
+  <div class="container mt-10">
+      <div class="flex justify-center">
+          <h5 class="flex-col">Index page</h5>
+          <router-link to="/register">
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  Click here to register
+              </button>
+          </router-link>
+      </div>
+  </div>
+</template>
+
 <script setup>
 
 </script>
-
-<template>
-  <div>About</div>
-</template>
 
 <style scoped>
 
